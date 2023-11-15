44c0538370cbb1142ac94cf50758fa722a93dc09
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-11-14 22:30:29 -0800

    Added countries to container for language choice

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const selectTag = document.querySelectorAll("select");
 
 selectTag.forEach(tag => {
     for (const country_code in countries) {
-    console.log(country_code)
+    console.log(countries[country_code])
+    let option = `<option value="${country_code}">${countries[country_code]}</option>`;
+    tag.insertAdjacentHTML("beforeend", option);
     }
 });
