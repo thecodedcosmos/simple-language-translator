a3375bc21fd71f08e7fa982a763b8550bbdf7efe
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-11-14 23:07:20 -0800

    Added and fetched apiUrl

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const selectTag = document.querySelectorAll("select");
+const fromText = document.querySelector(".from-text"),
+selectTag = document.querySelectorAll("select"),
+translateBtn = document.querySelector("button");
 
 selectTag.forEach((tag, id) => {
     for (const country_code in countries) {
@@ -11,4 +13,14 @@ selectTag.forEach((tag, id) => {
     let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
     tag.insertAdjacentHTML("beforeend", option);
     }
+});
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value,
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        console.log(data);
+    });
 });
